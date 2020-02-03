22a53bb41c33ec3d25f76c79426f292fba25f3be
br3y1n <br3y1n@hotmail.es>
2020-02-02 23:14:03 -0500

    Updates

---
+++ precache-manifest.963a3b14e590153c945d6b287eff162b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63581a5483aa8df73a55b406a71eb18e",
+    "url": "/globant-escape-game-2020/index.html"
+  },
+  {
+    "revision": "48a90841a6c040b62bb9",
+    "url": "/globant-escape-game-2020/static/css/main.7ba4fb92.chunk.css"
+  },
+  {
+    "revision": "ad01d31fc1568d98af70",
+    "url": "/globant-escape-game-2020/static/js/2.6684b157.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/globant-escape-game-2020/static/js/2.6684b157.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48a90841a6c040b62bb9",
+    "url": "/globant-escape-game-2020/static/js/main.dd02c7b7.chunk.js"
+  },
+  {
+    "revision": "b02a4e752f990690e26d",
+    "url": "/globant-escape-game-2020/static/js/runtime-main.478583dd.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/globant-escape-game-2020/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/globant-escape-game-2020/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "3674f5ca46c3d5986525acf33c4c6c65",
+    "url": "/globant-escape-game-2020/static/media/Timotheo.3674f5ca.ttf"
+  },
+  {
+    "revision": "9b75544e08f336d58b1c06482eb7a8ce",
+    "url": "/globant-escape-game-2020/static/media/logo.9b75544e.png"
+  }
+]);
