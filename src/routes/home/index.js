38d58256cbd467eb07d99b1ceeea7a50d4901ac6
br3y1n <brayan@plasticolab.com>
2020-02-05 11:18:17 -0500

    home page copy is changed

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -9,7 +9,7 @@ const Home = () => {
             <Img src={logo} />
             <H2>&raquo; Globant - Escape Game 2020</H2>
             <P>
-                This is a page where we, the "<strong>Sky team</strong>", conformed by <strong>Brayan Arango</strong> and <strong>Sebastian Salgado</strong>, developed a tool that facilitates the solution of the challenges proposed in the event Globant - Escape Game 2020.
+                This is a page developed in React.js (beginner - first App) where we, the "<strong>Sky team</strong>", conformed by <strong>Brayan Arango</strong> and <strong>Sebastian Salgado</strong>, implemented a tool that facilitates the solution of the challenges proposed in the event Globant - Escape Game 2020.
             </P>
             <P>
                 We invite you to try the tool and find the answers to the challenges with the help of it.
